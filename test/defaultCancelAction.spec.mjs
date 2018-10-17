f25730564c645e98d69d62c81c2bb6db33a40078
DavidBeale <David.Beale@BealeARTS.co.uk>
2018-10-17 09:19:38 +0100

    More tests

--- test/defaultCancelAction.spec.mjs
+++ test/defaultCancelAction.spec.mjs
@@ -3,41 +3,41 @@ import { expect } from 'chai';
 import defaultCancelAction from '../src/defaultCancelAction';
 
 describe('defaultCancelAction', () => {
-    it('rejects non-object actions', () => {
-        expect(defaultCancelAction([])).to.equal(false);
+  it('rejects non-object actions', () => {
+    expect(defaultCancelAction([])).to.equal(false);
 
-        expect(defaultCancelAction({
-            noType: ''
-        })).to.equal(false);
-    });
+    expect(defaultCancelAction({
+      noType: ''
+    })).to.equal(false);
+  });
 
-    it('rejects non-cancel actions', () => {
-        expect(defaultCancelAction({
-            type: 'MY_TYPE'
-        })).to.equal(false);
+  it('rejects non-cancel actions', () => {
+    expect(defaultCancelAction({
+      type: 'MY_TYPE'
+    })).to.equal(false);
 
-        expect(defaultCancelAction({
-            type: 'namespace/MY_TYPE'
-        })).to.equal(false);
+    expect(defaultCancelAction({
+      type: 'namespace/MY_TYPE'
+    })).to.equal(false);
 
-        expect(defaultCancelAction({
-            type: 'name/space/MY_TYPE'
-        })).to.equal(false);
-    });
+    expect(defaultCancelAction({
+      type: 'name/space/MY_TYPE'
+    })).to.equal(false);
+  });
 
-    it('matches basic cancel types', () => {
-        expect(defaultCancelAction({
-            type: 'CANCEL_MY_TYPE'
-        })).to.equal('MY_TYPE');
-    });
+  it('matches basic cancel types', () => {
+    expect(defaultCancelAction({
+      type: 'CANCEL_MY_TYPE'
+    })).to.equal('MY_TYPE');
+  });
 
-    it('matches namespaced cancel types', () => {
-        expect(defaultCancelAction({
-            type: 'namespace/CANCEL_MY_TYPE'
-        })).to.equal('namespace/MY_TYPE');
+  it('matches namespaced cancel types', () => {
+    expect(defaultCancelAction({
+      type: 'namespace/CANCEL_MY_TYPE'
+    })).to.equal('namespace/MY_TYPE');
 
-        expect(defaultCancelAction({
-            type: 'name/space/CANCEL_MY_TYPE'
-        })).to.equal('name/space/MY_TYPE');
-    });
+    expect(defaultCancelAction({
+      type: 'name/space/CANCEL_MY_TYPE'
+    })).to.equal('name/space/MY_TYPE');
+  });
 });
